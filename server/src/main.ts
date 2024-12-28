de3a5d618d06e0318ac8a8e8c7346ab926f0f0f9
David Bell-Brown <david.bellbrown@roksoftware.com>
2024-12-27 17:54:11 -0800

    make server buildable

--- server/src/main.ts
+++ server/src/main.ts
@@ -45,6 +45,9 @@ const postRouter = router({
   }),
 });
 
+const port = Number(process.env.PORT) || 2022;
+if (isNaN(port)) throw new Error("PORT environment variable must be a number");
+
 // Merge routers together
 const appRouter = router({
   greeting: greetingRouter,
@@ -56,7 +59,7 @@ export type AppRouterInput = inferRouterInputs<AppRouter>;
 export type AppRouterOutput = inferRouterOutputs<AppRouter>;
 
 const wss = new WebSocketServer({
-  port: 2022,
+  port,
 });
 const handler = applyWSSHandler({ wss, router: appRouter, createContext });
 
@@ -66,7 +69,7 @@ wss.on("connection", ws => {
     console.log(`➖➖ Connection (${wss.clients.size})`);
   });
 });
-console.log("✅ WebSocket Server listening on ws://localhost:2022");
+console.log("✅ WebSocket Server listening on 2022");
 
 process.on("SIGINT", () => {
   handler.broadcastReconnectNotification();
