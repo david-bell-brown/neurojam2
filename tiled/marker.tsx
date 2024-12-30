5474b1506381d59705d7e9fdb09c65ec42cc3386
Maxwell Terpstra <telic.detour+github@gmail.com>
2024-12-29 18:26:23 -0800

    start of tiled map data

---
+++ tiled/marker.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="marker" tilewidth="1" tileheight="1" tilecount="1" columns="0">
+ <editorsettings>
+  <export target="../server/src/assets/marker-tileset.json" format="json"/>
+ </editorsettings>
+ <tile id="0">
+  <image source="pixel.png" width="1" height="1"/>
+ </tile>
+</tileset>
